98ca87642e8f9a418bac7ba79301c50b1a5924b4
Nick Clark <nick@itsoftware.net.au>
2014-06-27 22:59:17 +1000

    Added in comments
    Provided detail in comments

--- index.php
+++ index.php
@@ -1,43 +1,76 @@
 <?php
-	// notes:
+	// ------------------------------------------------------------ 
+	// Copyright (c) 2014 Nick Clark
+	// ------------------------------------------------------------ 
 	
-	// constants
-	define("DB_HOST", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASS", "pass");
-	define("DB_DB", "database");
-	
+	// ------------------------------------------------------------
+	// Constants
+	// ------------------------------------------------------------
+		// Database Information
+			// Enter your hostname, username, password and the
+			// database you wish data to be inserted into here.
+	define("DB_HOST", "localhost"); // Database Hostname
+	define("DB_USER", "root");		// Database Username
+	define("DB_PASS", "pass");		// Database Password
+	define("DB_DB", "database");	// Which Database?
+			// Also, specify a table prefix here if one exists
 	define("TABLE_PREFIX", "");
 	
-	define("VEND_USER", "username");
-	define("VEND_PASS", "password");
-	
+		// Vend information
+			// Enter your vend username and password here
+	define("VEND_USER", "username"); // Vend Username
+	define("VEND_PASS", "password"); // Vend Password
+			// Change YOURSUBDOMAINHERE to your store's subdomain
+			// (the bit before .vendhq.com)
 	define("VEND_BASE_URL", "http://YOURSUBDOMAINHERE.vendhq.com/api/");
 	define("VEND_AUTH", VEND_USER . ":" . VEND_PASS);
 
+	// ------------------------------------------------------------
+	// Functions
+	// ------------------------------------------------------------
+	
+		// Get Request is the function that "GETs" information
+		// (such as products).
+		
+		// $vend_url is the URL you wish to get (e.g. products)
+		// $parameters is a single dimension array
+		//		(e.g. array('order_by=id', 'order_direction=DESC'))
 	function get_request($vend_url, $parameters = null) {
+		// Append the URL to the base 
 		$vend_url = VEND_BASE_URL . $vend_url;
+		
+		// Detect if there are parameters
 		if (!empty($parameters)) {
-			$vend_url .= "?";
+			// Begin parameters using standard get encoding
+			$vend_url .= "?"; 
+			// Append each parameter
 			foreach ($parameters as $parameter) {
 				$vend_url .= $parameter;
 				$vend_url .= "&";
 			}
 			
+			// Remove the last character (an Ampersand)
 			$vend_url = substr_replace($vend_url, "", -1);
 		}
 		
+		// Begin the cURL call
 		$curl = curl_init($vend_url);
 		
+		// Authenticate the user
 		curl_setopt($curl, CURLOPT_USERPWD, VEND_AUTH);
+		// Request JSON data (currently only JSON is supported)
 		curl_setopt($curl, CURLOPT_HTTPHEADER, array(
 			'Accept: application/json', 'Content-Type: application/json'));
+		// Return the data rathern than output it
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		
+		// Get the response
 		$response = curl_exec($curl);
 		
+		// Close the cURL call
 		curl_close($curl);
 		
+		// return the response
 		return $response;
 	}
 ?>
